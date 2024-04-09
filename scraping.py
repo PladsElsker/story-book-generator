c809803dd1ca4dc4027f847a7cbfe83ad6222af7
John-WL <lebel.johnw@gmail.com>
2024-04-09 02:26:21 -0400

    Refactor

--- scraping.py
+++ scraping.py
@@ -1,4 +1,3 @@
-import json
 from pathlib import Path
 from urllib.parse import urlparse
 from selenium import webdriver
@@ -10,6 +9,35 @@ import text_cleaning
 
 
 SCRAPED_NOVELS_DIRECTORY = Path("scraped_novels")
+
+
+def create_chrome_driver():
+    service = Service(executable_path=CHROMEDRIVER_PATH)
+    options = Options()
+    # options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    return webdriver.Chrome(service=service, options=options)
+
+
+def novel_scrape(driver: webdriver.Chrome, novel_page_url: str) -> None:
+    parsed_url = urlparse(novel_page_url)
+    # TODO: use the last scraped page instead if we already started scraping this novel
+    # Requires retrieving data from the already scraped novels
+    # Can we fetch database data? 
+    # We do it from the file system directly? 
+
+    scraper = NovelScraperFactory().create(driver, novel_page_url)
+    scraped_data = scraper.scrape()
+
+    # TODO: update the scraped data in the database
+
+
+class NovelScraperFactory:
+    def create(self, driver: webdriver.Chrome, novel_page_url: str) -> str:
+        parsed_url = urlparse(novel_page_url)
+        website = parsed_url.netloc.split(":")[0]
+        return SCRAPER_MAP[website](driver, novel_page_url)
 
 
 class NovelScraper:
@@ -25,7 +53,6 @@ class NovelScraper:
 class WebnovelScraper(NovelScraper):
     def __init__(self, driver: webdriver.Chrome, url: str) -> None:
         super().__init__(driver, url)
-        self.driver.execute_script("document.querySelector('#j_read').click();")
 
     def scrape(self) -> list[str]:
         end_reached = False
@@ -44,23 +71,23 @@ class WebnovelScraper(NovelScraper):
                         clearTimeout(timeout_handle);
                         callback('');
                     }
-                }, 1000);
+                }, 50);
                 timeout_handle = setTimeout(() => callback('done'), 4000);
             """)
         
-        chapters = json.loads(self.driver.execute_script("""
+        chapters = self.driver.execute_script("""
             const all_chapters = [...document.querySelectorAll('.chapter_content')];
             console.log("test-1-2");  
-            return JSON.stringify(all_chapters.map(chapter => {
+            return all_chapters.map(chapter => {
                 const title = chapter.querySelector(".cha-tit").textContent;
                 const paragraph = chapter.querySelector(".cha-words").textContent;
                 return {title, paragraph};
-            }));
-        """))
+            });
+        """)
         scraped = {}
         scraped["chapters"] = [text_cleaning.clean_chapter(chapter) for chapter in chapters]
-        scraped["main_page_url"] = self.url;
-        scraped["last_scraped_url"] = self.driver.execute_script("return window.location.href;");
+        scraped["main_page_url"] = self.url
+        scraped["last_scraped_url"] = self.driver.execute_script("return window.location.href;")
         return scraped
 
 
@@ -69,24 +96,9 @@ SCRAPER_MAP = {
 }
 
 
-class ScraperFactory:
-    def create(self, driver: webdriver.Chrome, novel_page_url: str, *, website: str = None) -> str:
-        if not website:
-            parsed_url = urlparse(novel_page_url).netloc
-            website = parsed_url
-
-        return SCRAPER_MAP[website](driver, novel_page_url)
-
-
 if __name__ == "__main__":
-    service = Service(executable_path=CHROMEDRIVER_PATH)
-    options = Options()
-    # options.add_argument("--headless")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = create_chrome_driver()
+    # novel_scrape(driver, "https://www.webnovel.com/book/mushoku-tensei-full-version_27096259406624705/volume-1-prologue_72736022055680334")
+    novel_scrape(driver, "https://www.webnovel.com/book/reincarnated-with-the-mind-control-powers-in-another-world._25331737205609705/chapter-1_67999384498920800")
 
-    # scraper = ScraperFactory().create(driver, "https://www.webnovel.com/book/mushoku-tensei-full-version_27096259406624705")
-    scraper = ScraperFactory().create(driver, "https://www.webnovel.com/book/strongest-mage-with-the-lust-system_22715595806121505")
-    scraped = scraper.scrape()
     driver.quit()
