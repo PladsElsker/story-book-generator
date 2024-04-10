6f5d0ec1e303c73052507ec6b25ebc25c77e12a3
John-WL <lebel.johnw@gmail.com>
2024-04-10 19:40:49 -0400

    Headless argument

--- scraper/chromedriver/driver.py
+++ scraper/chromedriver/driver.py
@@ -5,11 +5,13 @@ from selenium.webdriver.chrome.options import Options
 from .update import GOOGLE_CHROME_BINARY, CHROMEDRIVER_BINARY
 
 
-def create_chrome_driver() -> webdriver.Chrome:
+def create_chrome_driver(headless: bool = False) -> webdriver.Chrome:
     service = Service(executable_path=CHROMEDRIVER_BINARY)
     options = Options()
     options.binary_location = str(GOOGLE_CHROME_BINARY)
-    # options.add_argument("--headless")
+    if headless:
+        options.add_argument("--headless")
+    
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     return webdriver.Chrome(service=service, options=options)
