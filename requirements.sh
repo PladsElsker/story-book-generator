ad8d20ab88d17428b0dd97f8df4b7aab82db9ac9
John-WL <lebel.johnw@gmail.com>
2024-04-15 00:37:35 -0400

    requirements fix

--- requirements.sh
+++ requirements.sh
@@ -1,16 +1,13 @@
-install_venv() {
-    echo Installing dependencies in $1
-    cd $1 || { echo Failed to change directory to $1; exit 1; }
-    if [ ! -d venv ]; then
-        echo Creating a new virtual environment
-        python3 -m venv venv || { echo Failed to create virtual environment; exit 1; }
-    fi
-    venv/bin/pip install -r requirements.txt || { echo Failed to install requirements; exit 1; }
-    venv/bin/pip install -e ../novels || { echo Failed to install the novels package; exit 1; }
-    cd .. || { echo Failed to return to the parent directory; exit 1; }
+install_requirements() {
+    echo Installing dependencies for $1
+    venv/bin/pip install -r $1/requirements.txt || { echo Failed to install requirements; exit 1; }
 }
 
 
-install_venv scraper
-install_venv scenes
-install_venv audio
+if [ ! -d venv ]; then
+    echo Creating a new virtual environment
+    python3 -m venv venv || { echo Failed to create virtual environment; exit 1; }
+fi
+
+
+install_requirements scraper
