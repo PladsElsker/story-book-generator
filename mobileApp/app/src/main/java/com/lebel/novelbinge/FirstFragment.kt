d860b3d679e7a8a7d6982acae2c7ae528dc7dd71
xaviercl <xcl7992@hotmail.com>
2024-04-19 22:05:12 -0400

    Created the recycler view item, still fake data in and out, and untested

--- mobileApp/app/src/main/java/com/lebel/novelbinge/FirstFragment.kt
+++ mobileApp/app/src/main/java/com/lebel/novelbinge/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.lebel.novelbinge
 
+import NovelCardRecyclerViewAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.lebel.novelbinge.databinding.FragmentFirstBinding
 
 /**
@@ -23,18 +25,18 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        val dataset = arrayOf("January", "February", "March")
+        val customAdapter = NovelCardRecyclerViewAdapter(dataset)
+
+        val recyclerView: RecyclerView? = getView()?.findViewById(R.id.novels)
+        recyclerView?.adapter = customAdapter
     }
 
     override fun onDestroyView() {
---
+++ mobileApp/app/src/main/java/com/lebel/novelbinge/novelCard/NovelCardRecyclerViewAdapter.kt
@@ -0,0 +1,44 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.lebel.novelbinge.R
+
+class NovelCardRecyclerViewAdapter(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<NovelCardRecyclerViewAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View
+            textView = view.findViewById(R.id.textView)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.novel_card_recycler_view, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = dataSet[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
